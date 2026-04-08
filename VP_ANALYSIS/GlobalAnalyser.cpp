38ec544ebe2ae5da91b276d5e24b7d337b54ed06
sweet12prof <chrisnut12@gmail.com>
2026-04-08 03:16:46 +0000

    Scc Filtering, Window Hashing and Hashed Window Distribution

--- VP_ANALYSIS/GlobalAnalyser.cpp
+++ VP_ANALYSIS/GlobalAnalyser.cpp
@@ -8,7 +8,7 @@
 void GlobalAnalyser_FT::fill_graph(std::vector<TraceData>& traceChunk,
                                    Vertex_Property vertex_property) {
   for (UINT64 i{0}; i < traceChunk.size(); i++) {
-    auto value = extract_vertex_from_trace(traceChunk, i, vertex_property);
+    auto value = extract_value_from_trace(traceChunk, i, vertex_property);
     auto& value_index = value_to_vertex_table.get<addrint_tag>();
     auto it = value_index.find(value);
     if (it == value_index.end()) {
@@ -21,9 +21,9 @@ void GlobalAnalyser_FT::fill_graph(std::vector<TraceData>& traceChunk,
 
   for (size_t i = 0; i + 1 < traceChunk.size(); ++i) {
     auto curr_property =
-        extract_vertex_from_trace(traceChunk, i, vertex_property);
+        extract_value_from_trace(traceChunk, i, vertex_property);
     auto next_property =
-        extract_vertex_from_trace(traceChunk, i + 1, vertex_property);
+        extract_value_from_trace(traceChunk, i + 1, vertex_property);
 
     auto& value_index = value_to_vertex_table.get<addrint_tag>();
     auto curr_vertex = value_index.find(curr_property);
@@ -53,8 +53,8 @@ void GlobalAnalyser_FT::fill_graph(std::vector<TraceData>& traceChunk,
 void GlobalAnalyser_FT::print_self_loop_info(std::ostream* out) const {
   for (auto item : boost::make_iterator_range(edges(g))) {
     (*out) << (void*)g[boost::source(item, g)].value << "->"
-           << (void*)g[boost::target(item, g)].value << "\t:" << "["
-           << g[item].self_loop_count << "]" << std::endl;
+           << (void*)g[boost::target(item, g)].value << "\t:"
+           << "[" << g[item].self_loop_count << "]" << std::endl;
   }
 }
 
@@ -83,8 +83,8 @@ void GlobalAnalyser_FT::print_value_frequency(std::ostream* out) const {
   }
 }
 
-UINT64 extract_vertex_from_trace(std::vector<TraceData>& traceChunk,
-                                 UINT64 trace_index, Vertex_Property property) {
+UINT64 extract_value_from_trace(std::vector<TraceData>& traceChunk,
+                                UINT64 trace_index, Vertex_Property property) {
   switch (property) {
     case Vertex_Property::PC:
       return traceChunk[trace_index].pc;
@@ -97,16 +97,93 @@ UINT64 extract_vertex_from_trace(std::vector<TraceData>& traceChunk,
   }
 }
 
-void generate_window_hashes(std::vector<TraceData>& traceChunk,
-                            std::size_t window_length, UINT64& hash, std::) {}
-
-UINT64 generate_initial_hash(std::vector<TraceData>& traceChunk,
-                             std::size_t window_length,
-                             Vertex_Property property) {
-  UINT64 hash{0};
-  for (std::size_t i{0}; i < window_length; i++) {
-    auto value = extract_vertex_from_trace(traceChunk, i, property);
-    hash += value * constants_table[i];
+void GlobalAnalyser_FT::hash_windows(std::vector<TraceData>& traceChunk,
+                                     std::size_t window_length,
+                                     Vertex_Property property,
+                                     std::vector<TraceData>& leftOver) {
+  std::deque<ADDRINT> window;
+  for (size_t i{0}; i < window_length; i++) {
+    window.push_back(extract_value_from_trace(traceChunk, i, property));
   }
-  return hash;
+  // generate hash for first window
+  auto hash = generate_initial_hash(traceChunk, window_length, property);
+  // check if first window is an SCC member
+  auto is_member = GlobalAnalyser_FT::is_window_in_scc(window);
+  if (is_member) {
+    window_freq_map[hash].frequency++;
+    window_freq_map[hash].first_element = window.front();
+  }
+
+  UINT64 first_term{1};
+  for (size_t i{0}; i < window_length - 1; i++) first_term *= POLY_BASE;
+
+  for (size_t i{0}; i < traceChunk.size() - window_length; i++) {
+    // get property; mem address or pc
+    if (i + window_length >= traceChunk.size()) break;
+    auto new_window_element =
+        extract_value_from_trace(traceChunk, i + window_length, property);
+
+    // generate new hash
+    hash -= window.front() * first_term;
+    // get next window
+    window.pop_front();
+    window.push_back(new_window_element);
+    hash *= POLY_BASE;
+    hash += new_window_element;
+
+    is_member = is_window_in_scc(window);
+    if (is_member) {
+      auto& entry = window_freq_map[hash];
+      if (entry.frequency == 0) entry.first_element = window.front();
+      // assert(window_freq_map[hash].first_element == window.front() &&
+      //        "COLLISION OCCURED");
+      entry.frequency++;
+    }
+  }
+  size_t remainder = (traceChunk.size() < window_length) ? traceChunk.size()
+                                                         : window_length - 1;
+  leftOver.clear();
+  if (remainder > 0) {
+    leftOver.insert(leftOver.end(), (traceChunk.end() - remainder),
+                    traceChunk.end());
+  }
+}
+
+void GlobalAnalyser_FT::add_single_transition(TraceData& curr_element,
+                                              TraceData& next_element,
+                                              Vertex_Property property) {
+  auto curr_value =
+      property == MEM_ADDRESS ? curr_element.memAddress : curr_element.pc;
+
+  auto next_value =
+      property == MEM_ADDRESS ? next_element.memAddress : next_element.pc;
+
+  auto& value_index = value_to_vertex_table.get<addrint_tag>();
+  auto next_vertex = value_index.find(next_value);
+  Graph::vertex_descriptor vid;
+  if (next_vertex == value_index.end()) {
+    vid = boost::add_vertex({next_value, 1}, g);
+    value_to_vertex_table.insert({vid, next_value});
+  } else {
+    g[next_vertex->vertex_id].value_frequency++;
+    vid = next_vertex->vertex_id;
+  }
+
+  auto curr_vertex = value_index.find(curr_value);
+
+  assert(curr_vertex != value_index.end() &&
+         "All vertices and their corresponding value must be in table");
+
+  auto [e, exists] = boost::edge(curr_vertex->vertex_id, vid, g);
+
+  if (!exists) {
+    bool inserted;
+    boost::tie(e, inserted) = boost::add_edge(curr_vertex->vertex_id, vid, g);
+    g[e].self_loop_count = 0;
+  }
+
+  // THIS IS THE IMPORTANT PART
+  if (curr_vertex->vertex_id == vid) {
+    g[e].self_loop_count++;
+  }
 }
--- VP_ANALYSIS/GlobalAnalyser.hpp
+++ VP_ANALYSIS/GlobalAnalyser.hpp
@@ -1,23 +1,44 @@
+#include <deque>
 #include <iostream>
+#include <map>
 
 #include "fileprocess.hpp"
 
 #ifndef GLOBALANALYSER_H
 #define GLOBALANALYSER_H
 
-UINT64 extract_vertex_from_trace(std::vector<TraceData>& traceChunk,
-                                 UINT64 trace_index, Vertex_Property property);
+struct window_info {
+  UINT64 frequency;
+  ADDRINT first_element;
+};
 
-// sliding Window Hash
-UINT64 generate_window_hashes(std::vector<TraceData>& traceChunk,
-                              std::size_t window_length);
-UINT64 generate_initial_hash(std::vector<TraceData>& traceChunk,
-                             std::size_t window_length);
+UINT64 extract_value_from_trace(std::vector<TraceData>& traceChunk,
+                                UINT64 trace_index, Vertex_Property property);
+
+// // sliding Window Hash
+// std::vector<std::vector<TraceData>> generate_windows(
+//     std::vector<TraceData>& traceChunk, std::vector<TraceData>& leftOver,
+//     size_t window_size);
+
+inline UINT64 generate_initial_hash(std::vector<TraceData>& traceChunk,
+                                    std::size_t window_length,
+                                    Vertex_Property property) {
+  UINT64 hash{0};
+  for (std::size_t i{0}; i < window_length; i++) {
+    auto value = extract_value_from_trace(traceChunk, i, property);
+    hash += value * constants_table[i];
+  }
+  return hash;
+}
 
 class GlobalAnalyser_FT {
  public:
   void fill_graph(std::vector<TraceData>& traceChunk,
                   Vertex_Property vertex_property);
+
+  void add_single_transition(TraceData& curr_element, TraceData& next_element,
+                             Vertex_Property property);
+
   void generate_strongly_connected_componenets();
 
   /*              VALIDATE SLIDING WINDOWS                  */
@@ -29,15 +50,51 @@ class GlobalAnalyser_FT {
   /*      GENERATE AND PRE-FILTER WINDOWS, FREQUENCY         */
   std::vector<std::vector<UINT64>> generate_sliding_windows();
 
+  // void generate_window_distribution(std::vector<TraceData>& traceChunk,
+  //                                   size_t window_size);
+
   void print_self_loop_info(std::ostream* out) const;
   void print_value_frequency(std::ostream* out) const;
   void print_strongly_connected_components(std::ostream* out) const;
+
+  void print_window_hash_map_freq() const {
+    for (auto item : window_freq_map) {
+      std::cout << "Hash: " << item.first
+                << " Frequency: " << item.second.frequency << std::endl;
+    }
+  }
+
+  bool is_window_in_scc(std::deque<ADDRINT>& window_sequence) {
+    if (window_sequence.empty()) return false;
+    auto& value_index = value_to_vertex_table.get<addrint_tag>();
+    auto it = value_index.find(window_sequence[0]);
+    assert(it != value_index.end() &&
+           "All elements must be in value_to_vertex_table");
+    auto first_component = component_result_pair.second[it->vertex_id];
+    // if element does not recur no need to consider window
+    if (g[it->vertex_id].value_frequency == 1) return false;
+    for (std::size_t i{1}; i < window_sequence.size(); i++) {
+      it = value_index.find(window_sequence[i]);
+      assert(it != value_index.end() &&
+             "All elements must be in value_to_vertex_table");
+      // if element does not recur no need to consider window
+      if (g[it->vertex_id].value_frequency == 1) return false;
+      if (component_result_pair.second[it->vertex_id] != first_component)
+        return false;
+    }
+    return true;
+  };
+
+  void hash_windows(std::vector<TraceData>& traceChunk,
+                    std::size_t window_length, Vertex_Property property,
+                    std::vector<TraceData>& leftOver);
 
  private:
   Graph g;
   // address to vertex ID map
   std::pair<INT64, std::vector<INT64>> component_result_pair;
   Value_To_Vertex_Container value_to_vertex_table;
+  std::map<UINT64, window_info> window_freq_map;
 };
 
 #endif
